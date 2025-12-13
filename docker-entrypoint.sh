c9ed1ba7f540afa6fcb68e97cdfd1bda977509a6
D.C. Compton <dc@compton.fyi>
2025-12-13 08:26:03 -0600

    Try as normal.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,6 @@ echo "MEDPLUM_BASE_URL: ${MEDPLUM_BASE_URL}"
 # Find all files in the assets directory and replace placeholders
 find /usr/share/caddy/assets -type f -exec sed -i \
   -e "s|__MEDPLUM_BASE_URL__|${MEDPLUM_BASE_URL}|g" \
-  -e "s|http://localhost:8103|${MEDPLUM_BASE_URL}|g" \
   -e "s|__MEDPLUM_CLIENT_ID__|${MEDPLUM_CLIENT_ID}|g" \
   -e "s|__GOOGLE_CLIENT_ID__|${GOOGLE_CLIENT_ID}|g" \
   -e "s|__RECAPTCHA_SITE_KEY__|${RECAPTCHA_SITE_KEY}|g" \
